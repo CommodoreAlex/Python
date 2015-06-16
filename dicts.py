57fc1df4a6d996a66b6f37f977d6862d77f6b156
Alexander Commodore <alexandercommodore@gmail.com>
2015-06-16 08:54:31 -0400

    Dictionaries
    Just some information on dictionaries

---
+++ dicts.py
@@ -0,0 +1,16 @@
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+
+#del dict['Class'];
+#dict.clear(); #Removes, clears, or deletes the entire dictionary
+#del dict ;
+
+print "['Name']: ", dict['Name']; #Prints the dictionary items V
+print "['Age']: ", dict['Age'];
+print "['Class']: ", dict['Class'];
+
+print str((dict)) #Prints dictionary as a string
+print len((dict)) #Prints out the length of a dictionary
+print type((dict)) #Prints out the type of dictionary
+#dict.copy() #Copys the dictionary
+print dict.values() #Returns a list of dict's values
+print dict.items() #Returns a list of the items in the dictionary
